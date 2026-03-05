53c435fe8308dcb9380668f19f3fc3c483c5fe1f
hiralsinghal <singhal.hiral@gmail.com>
2026-03-06 01:33:30 +0530

    workingg

--- script.js
+++ script.js
@@ -9,12 +9,26 @@ function getCharacter(e) {
         const nameH2 = document.querySelector("#displayCharName");
         const hairP = document.querySelector("#displayHairColor");
         const heightP = document.querySelector("#displayHeight");
-        const massP = document.querySelector("displayMass");
+        const massP = document.querySelector("#displayMass");
+        const eyeP = document.querySelector("#displayEyeColor");
+        const yearP = document.querySelector("#displayBirthYear");
+        const genderP = document.querySelector("#displayGender");
+        const homeworldP = document.querySelector("#displayHomeworld");        
 
         nameH2.textContent = data.results[0].name;
         hairP.textContent = `Hair Color: ${data.results[0].hair_color}`;
         heightP.textContent = `Height: ${data.results[0].height}`;
         massP.textContent = `Mass: ${data.results[0].mass}`;
+        eyeP.textContent = `Eye Color: ${data.results[0].eye_color}`;
+        yearP.textContent = `Birth Year: ${data.results[0].birth_year}`;
+        genderP.textContent = `Gender: ${data.results[0].gender}`;
+        
+        fetch(data.results[0].homeworld)
+        .then((response)=>response.json())
+        .then((planetData)=>{
+            homeworldP.textContent = `Homeworld: ${planetData.results[0].homeworld}`;
+        })
+        
     })
 
     .catch((err)=>{
