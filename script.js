e57ab8bbf1cc11fe11d11f94cf18bb6af6b37687
hiralsinghal <singhal.hiral@gmail.com>
2026-03-06 01:16:16 +0530

    yayyy

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ function getCharacter(e) {
     fetch(`https://swapi.dev/api/people/?search=${name}`)
     .then((response)=>response.json())
     .then((data)=>{
-        const nameH2 = document.querySelector("#displayCharName")
+        const nameH2 = document.querySelector("#displayCharName");
+        const hairP = document.querySelector("#displayHairColor");
+        const heightP = document.querySelector("#displayHeight");
+        const massP = document.querySelector("displayMass");
 
         nameH2.textContent = data.results[0].name;
+        hairP.textContent = `Hair Color: ${data.results[0].hair_color}`;
+        heightP.textContent = `Height: ${data.results[0].height}`;
+        massP.textContent = `Mass: ${data.results[0].mass}`;
     })
 
     .catch((err)=>{
-        console.log("Character not found",err)
+        console.log("Character not found",err);
     })
 
     e.preventDefault()
